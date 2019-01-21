4c231b95c666ec3a9a14a9ece0fae05a7f58301a
amazing T <>
2019-01-21 22:59:52 +0800

    simple-redux-store

--- server/src/store/index.js
+++ server/src/store/index.js
@@ -5,7 +5,7 @@ const reducer = (state = { name: 'dell' }, action) => {
   return state
 }
 
-// 每个用户反问，都创建一个新的store
+// 每个用户访问，都创建一个新的store
 const getStore = () => {
   return createStore(reducer, applyMiddleware(thunk))
 }
